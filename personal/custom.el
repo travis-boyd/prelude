db16fa654261c5a5b1d35b4380b26f753ba51014
Travis Boyd <TravisClarkBoyd@gmail.com>
2022-12-07 22:26:29 -0500

    added personal/ el files

---
+++ personal/custom.el
@@ -0,0 +1,23 @@
+(org-babel-do-load-languages
+ 'org-babel-load-languages '((C . t)))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-capture-templates
+   '(("tt" "Todo template" entry
+      (file "~/org/notes.org")
+      "* TODO %?\\n:PROPERTIES:\\n:Via:\\n:Note:\\n:END:\\n:LOGBOOK:\\n- State \\\"TODO\\\"\\tfrom \\\"\\\"\\t%U\\n:END:")
+     ("t" "Thought for the drafts folder" entry
+      (file "~/org/thoughts.org")
+      "")))
+ '(package-selected-packages
+   '(use-package org-roam yaml-mode web-mode company-anaconda anaconda-mode lsp-ui lsp-mode json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company helm-ag helm-descbinds helm-projectile helm counsel swiper ivy exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text projectile imenu-anywhere hl-todo guru-mode git-modes git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ personal/personal-modules.el
@@ -0,0 +1,24 @@
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory (file-truename "~/org-roam/"))
+  :bind (("C-c m l" . org-roam-buffer-toggle)
+         ("C-c m f" . org-roam-node-find)
+         ("C-c m g" . org-roam-graph)
+         ("C-c m i" . org-roam-node-insert)
+         ("C-c m c" . org-roam-capture)
+         ;; Dailies
+         ("C-c m j" . org-roam-dailies-capture-today))
+  :config
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol))
+(setq org-roam-capture-templates
+      '(("d" "default" plain "%?"
+         :if-new
+         (file+head "${slug}.org"
+                    "#+title: ${title}\n#+date: %u\n#+lastmod: \n\n")
+         ))
+      time-stamp-start "#\\+lastmod: [\t]*")
